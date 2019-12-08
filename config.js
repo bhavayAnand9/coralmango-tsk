f05873f8043b68a7c09cb38a92f7a2773d896a88
bhavayAnand9 <bhavayanandcse@gmail.com>
2019-12-08 20:47:53 +0530

    changed port

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    PORT: 8080,
+    PORT: 8000,
     MONGODB_URI: "mongodb://lll66:lIOTfmuzRSlFFbzZqiQgeHRvc5G7il2NhBQdAFVM0We2AwlfEKzLQAQ2fp9DMzmOLthYy8vHyMdnvEVLZrZe1w==@lll66.documents.azure.com:10255/?ssl=true",
     SECRET_KEY: "coral mongo task bhavay anand"
 };
